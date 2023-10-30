fe0a4b7ad9e75e7f904b071bc894243dda34092d
Aleš Razým <ales.razym@gmail.com>
2023-10-30 10:59:31 +0100

    Update to API 34 to make it compatible, additional file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "cz.quanti.razym.rocketapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "cz.quanti.razym.rocketapp"
