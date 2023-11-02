ff75e7b336bfc04fab8a301f6d63c14fc8b784c9
Aleš Razým <ales.razym@quanti.cz>
2023-11-02 09:43:14 +0100

    FEAT: Configure `OkHttpClient` with (reasonable) timeout

--- app/src/main/java/cz/quanti/razym/rocketapp/di/RocketModule.kt
+++ app/src/main/java/cz/quanti/razym/rocketapp/di/RocketModule.kt
@@ -6,6 +6,7 @@ import cz.quanti.razym.rocketapp.data.RocketsRepositoryImpl
 import cz.quanti.razym.rocketapp.data.SpaceXApi
 import cz.quanti.razym.rocketapp.domain.RocketsRepository
 import cz.quanti.razym.rocketapp.presentation.RocketListViewModel
+import okhttp3.OkHttpClient
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
@@ -13,11 +14,13 @@ import org.koin.dsl.module
 import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 val rocketModule = module {
+    single { provideClient() }
     single { provideMoshi() }
     single { provideConverterFactory(get()) }
-    single { provideRetrofit(get()) }
+    single { provideRetrofit(get(), get()) }
     single { provideSpaceXApi(get()) }
     singleOf(::RocketsRepositoryImpl) { bind<RocketsRepository>() }
     viewModelOf(::RocketListViewModel)
@@ -33,9 +36,16 @@ private fun provideConverterFactory(moshi: Moshi): Converter.Factory {
     return MoshiConverterFactory.create(moshi)
 }
 
-private fun provideRetrofit(converter: Converter.Factory): Retrofit {
+private fun provideClient(): OkHttpClient {
+    return OkHttpClient.Builder()
+        .callTimeout(5, TimeUnit.SECONDS)
+        .build()
+}
+
+private fun provideRetrofit(client: OkHttpClient, converter: Converter.Factory): Retrofit {
     return Retrofit.Builder()
         .baseUrl("https://api.spacexdata.com/")
+        .client(client)
         .addConverterFactory(converter)
         .build()
 }
