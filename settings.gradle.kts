e7f7ad871848ffc6c92123fd31991cec6c77e8fb
Aleš Razým <ales.razym@quanti.cz>
2023-11-08 15:07:04 +0100

    FEAT: Add check for correct version of Java used to run gradle with hints for possible fixes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,3 +17,12 @@ dependencyResolutionManagement {
 
 rootProject.name = "rocket-app"
 include(":app")
+
+check(JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_17)) {
+    "Incompatible Java, need to run with Java 17, but found ${JavaVersion.current()}\n\n" +
+    "If you are using AndroidStudio, check the settings in File -> Settings -> Build, Execution, Deployment -> Build Tools -> Gradle -> Gradle JDK\n\n" +
+    "If you run from command line, check your gradle.properties file in project root and in [user home]/.gradle, and make sure it contains:\n" +
+    "org.gradle.java.home=/path/to/jdk-17\n\n" +
+    "Alternatively, you can force use of specific JDK using command line param:\n" +
+    "gradlew -Dorg.gradle.java.home=/path/to/jdk-17"
+}
