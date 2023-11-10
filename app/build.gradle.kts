f8c4f7bb556cbcd95612bbe08a087bf8af8aea25
Aleš Razým <ales.razym@quanti.cz>
2023-11-10 16:02:55 +0100

    REF: Migrate to Gradle version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,19 @@
+// See top level gradle for info on suppression.
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("androidx.navigation.safeargs.kotlin")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.androidx.navigationSafeArgsKotlin)
 }
 
 android {
     namespace = "cz.quanti.razym.rocketapp"
-    compileSdk = 34
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "cz.quanti.razym.rocketapp"
-        minSdk = 26
-        targetSdk = 34
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
@@ -42,7 +44,7 @@ android {
         viewBinding = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.2"
+        kotlinCompilerExtensionVersion = libs.versions.kotlinCompilerExtensionVersion.get()
     }
     packaging {
         resources {
@@ -52,48 +54,21 @@ android {
 }
 
 dependencies {
-    val koinVersion = "3.5.0"
-    val retrofitVersion = "2.9.0"
-    val navVersion: String by rootProject.extra
+    implementation(libs.androidx.coreKtx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.fragmentKtx)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.swipeRefreshLayout)
+    implementation(libs.android.material)
+    implementation(libs.bundles.lifecycle)
+    implementation(platform(libs.androidx.composeBom))
+    implementation(libs.bundles.compose)
+    implementation(libs.bundles.navigation)
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("io.insert-koin:koin-android:$koinVersion")
-    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
-    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
-    implementation("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
-    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
-    // As we are not going to test UI, we use Coil default singleton.
-    // If we need DI, use `io.coil-kt:coil-base` instead and configure loader(s) ourselves.
-    implementation("io.coil-kt:coil:2.5.0")
+    implementation(libs.koinAndroid)
+    implementation(libs.moshiKotlin)
+    implementation(libs.bundles.retrofit)
+    implementation(libs.coil)
 
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("io.mockk:mockk:1.13.8")
-    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
-    testImplementation("io.insert-koin:koin-test-junit4:$koinVersion")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    testImplementation(libs.bundles.test)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,27 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+// As per doc https://developer.android.com/build/migrate-to-catalogs#migrate-plugins
+//  If you are using a version of Gradle below 8.1, you need to annotate the plugins{} block with
+//  @Suppress("DSL_SCOPE_VIOLATION") when using version catalogs.
+//  Refer to issue #22797 https://github.com/gradle/gradle/issues/22797 for more info.
+
+@file:Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.androidx.navigationSafeArgsKotlin) apply false
 }
 
+// TODO: Don't know if this buildscript is needed, or not.
+//  Anyway, this will be removed by compose navigation.
+// Project compiles without it, but there is no relevant (lib catalog) documentation on
+// https://developer.android.com/guide/navigation/use-graph/safe-args#kts
+// In some repositories on github, it's only added to libs.versions.kts, not to buildscript {}.
 buildscript {
-    val navVersion by rootProject.extra { "2.7.5" }
-
     repositories {
         google()
     }
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+        classpath(libs.androidx.navigationSafeargsPlugin)
     }
 }
