f2efde64a37e5dd85c231196c3f9a8d1da4391b7
Aleš Razým <ales.razym@quanti.cz>
2023-11-22 15:25:05 +0100

    REF: Rfc rocket launch fragment (placeholder) to compose

--- app/src/main/java/cz/quanti/razym/rocketapp/system/RocketDetailFragment.kt
+++ app/src/main/java/cz/quanti/razym/rocketapp/system/RocketDetailFragment.kt
@@ -180,7 +180,7 @@ class RocketDetailFragment : Fragment() {
                         )
                     }
                 )
-            }
+            },
         )
     }
 
--- app/src/main/java/cz/quanti/razym/rocketapp/system/RocketLaunchFragment.kt
+++ app/src/main/java/cz/quanti/razym/rocketapp/system/RocketLaunchFragment.kt
@@ -4,32 +4,95 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import cz.quanti.razym.rocketapp.databinding.FragmentRocketLaunchBinding
+import cz.quanti.razym.rocketapp.R
+import cz.quanti.razym.rocketapp.ui.theme.RocketappTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class RocketLaunchFragment : Fragment() {
-
-    private var _binding: FragmentRocketLaunchBinding? = null
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentRocketLaunchBinding.inflate(inflater, container, false)
-
-        binding.toolbar.setNavigationOnClickListener {
-            findNavController().navigateUp()
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+            )
+            setContent {
+                RocketappTheme {
+                    RocketLaunchFragmentContent()
+                }
+            }
         }
-
-        binding.rocketDetailText.text = "Rocket Launch"
-
-        return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    @Preview
+    @Composable
+    private fun RocketLaunchFragmentContent() {
+        Scaffold(
+            topBar = {
+                RocketLaunchFragmentTopBar(stringResource(R.string.launch))
+            }
+        ) { innerPadding ->
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+            ) {
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.Center),
+                    text = "Rocket Launch",
+                    style = MaterialTheme.typography.titleMedium,
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun RocketLaunchFragmentTopBar(
+        title: String,
+    ) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+            },
+            navigationIcon = {
+                Icon(
+                    modifier = Modifier
+                        .size(36.dp)
+                        .padding(8.dp)
+                        .clickable {
+                            findNavController().navigateUp()
+                        },
+                    painter = painterResource(id = R.drawable.ic_arrow_back),
+                    // TODO
+                    contentDescription = "",
+                )
+            },
+        )
     }
 }
