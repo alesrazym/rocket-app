92e3e3f24c97922b05df7b7345c06905c6622a48
Aleš Razým <ales.razym@quanti.cz>
2023-12-05 11:41:42 +0100

    FEAT: Code style using `io.gitlab.arturbosch.detekt 1.23.4`.
    Add `io.nlopez.compose.rules:detekt:0.3.8` rules.
    Add `com.autonomousapps.dependency-analysis` plugin to check dependencies health (e.g. unused dependencies).
    Activate `detekt` plugin in Android Studio.
    Add `detekt.yml` rules to IDEA detekt plugin configuration.
    Ignore `@Composable` method naming (twitter's `TwitterCompose.ComposableNaming` applies.
    Do not run libraries rules - turn off.
    Force trailing comma.
    Add `detekt.md` readme.
    Add Gradle plugins `kotlinter-gradle` and `ktlint-gradle` (not used now - slower).
    Configure detekt baseline.
    Add `ij_*` rules to `.editorconfig` to have the same code style on all local machines.
    Note, that there still may be some rules missing, there were two rules found that have not been exported by IDE

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,40 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.detekt) apply false
+//    alias(libs.plugins.ktlintGradle) apply false
+    alias(libs.plugins.kotlinterGradle) apply false
+    alias(libs.plugins.dependencyAnalysis)
+}
+
+buildscript {
+    // TODO: It would be nice to be able to setup kotlinter within convention plugin.
+    //  Configuration with convention plugin is possible for ktlint gradle
+    // kotlinter force rules version.
+    configurations.classpath {
+        resolutionStrategy {
+            force(
+                libs.ktlint.rule.engine,
+                libs.ktlint.rule.engine.core,
+                libs.ktlint.cli.reporter.core,
+                libs.ktlint.cli.reporter.checkstyle,
+                libs.ktlint.cli.reporter.json,
+                libs.ktlint.cli.reporter.html,
+                libs.ktlint.cli.reporter.plain,
+                libs.ktlint.cli.reporter.sarif,
+                libs.ktlint.ruleset.standard,
+            )
+        }
+    }
+
+    // kotlinter add rules.
+    dependencies {
+        classpath(libs.ktlint.rule.compose)
+    }
+}
+
+dependencyAnalysis {
+    // TODO configuration goes here.
+    //  Also, configure when to run `buildHealth` task.
+    //  Now, this plugin is here as a reminder it exists and may be worth of use.
 }
