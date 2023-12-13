d475996c754855bccc054103293f0c61559fd850
Aleš Razým <ales.razym@quanti.cz>
2023-12-13 07:47:53 +0100

    Fix: Replace `material.Text` with `material3.Text`

--- app/src/main/java/cz/quanti/razym/rocketapp/system/RocketDetailScreen.kt
+++ app/src/main/java/cz/quanti/razym/rocketapp/system/RocketDetailScreen.kt
@@ -21,13 +21,13 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Text
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
