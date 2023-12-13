a6ec596fd60f4c6bb03abf6599d57525a741f5a1
Aleš Razým <ales.razym@quanti.cz>
2023-12-13 06:57:08 +0100

    Fix: Use `RocketappTheme` in `ContentStatusText`

--- app/src/main/java/cz/quanti/razym/rocketapp/ui/ContentStatusText.kt
+++ app/src/main/java/cz/quanti/razym/rocketapp/ui/ContentStatusText.kt
@@ -5,14 +5,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import cz.quanti.razym.rocketapp.ui.theme.RocketappTheme
 
 @Composable
@@ -23,13 +21,13 @@ fun ContentStatusText(
 ) {
     Text(
         text = stringResource(text),
-        style = MaterialTheme.typography.bodyLarge,
+        style = RocketappTheme.typography.bodyLarge,
         modifier =
             modifier
                 .fillMaxSize()
                 .wrapContentSize(Alignment.Center)
                 .clickable { onClick() }
-                .padding(16.dp),
+                .padding(RocketappTheme.dimens.extraLargePadding),
         textAlign = TextAlign.Center,
         color = RocketappTheme.colors.onBackground,
     )
