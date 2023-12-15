93a2ae0491deb922acd306649d62f98f56462284
Aleš Razým <ales.razym@quanti.cz>
2023-12-15 10:50:44 +0100

    REF: Split `Theme` into Colors.kt, Dimens.kt, Shapes.kt and Typography.kt

---
+++ ui/src/main/java/cz/quanti/razym/rocketapp/ui/theme/Shapes.kt
@@ -0,0 +1,16 @@
+package cz.quanti.razym.rocketapp.ui.theme
+
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.runtime.Immutable
+
+@Immutable
+class Shapes(
+    // Copy from MaterialTheme (commented out not used in app now).
+    // Shapes None and Full are omitted as None is a RectangleShape and Full is a CircleShape.
+//    val extraSmall: CornerBasedShape = ShapeDefaults.ExtraSmall,
+//    val small: CornerBasedShape = ShapeDefaults.Small,
+    val medium: CornerBasedShape = ShapeDefaults.Medium,
+//    val large: CornerBasedShape = ShapeDefaults.Large,
+//    val extraLarge: CornerBasedShape = ShapeDefaults.ExtraLarge,
+)
