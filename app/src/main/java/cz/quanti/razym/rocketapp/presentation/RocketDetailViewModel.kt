bbd66769d1b36778dfffa49970f714e7d617b783
Aleš Razým <ales.razym@quanti.cz>
2023-12-21 15:32:59 +0100

    FEAT: Add UseCase base interface(s) and `getRocket` and `getRockets` implementations

--- app/src/main/java/cz/quanti/razym/rocketapp/presentation/RocketDetailViewModel.kt
+++ app/src/main/java/cz/quanti/razym/rocketapp/presentation/RocketDetailViewModel.kt
@@ -6,13 +6,13 @@ import androidx.lifecycle.viewModelScope
 import cz.quanti.razym.rocketapp.R
 import cz.quanti.razym.rocketapp.asResult
 import cz.quanti.razym.rocketapp.model.asRocketDetail
-import cz.quanti.razym.rocketropository.domain.RocketsRepository
+import cz.quanti.razym.rocketropository.domain.GetRocketUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class RocketDetailViewModel(
-    private val repo: RocketsRepository
+    private val getRocketUseCase: GetRocketUseCase,
 ) : ViewModel() {
     private val _uiState: MutableStateFlow<UiScreenState<RocketDetailUiState>> =
         MutableStateFlow(UiScreenState.Loading(UiText.StringResource(R.string.getting_rocket_detail_in_progress)))
@@ -33,7 +33,7 @@ class RocketDetailViewModel(
 
     fun fetchRocket(id: String) {
         viewModelScope.launch {
-            repo.getRocket(id)
+            getRocketUseCase(id)
                 .asResult()
                 .update(
                     uiState = _uiState,
