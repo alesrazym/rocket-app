175bc1bbd06fe70ed2897f8b055cdb88faf8646c
Aleš Razým <ales.razym@quanti.cz>
2023-12-21 15:35:32 +0100

    Code style

--- RocketRepository/src/commonMain/kotlin/cz/quanti/razym/rocketropository/data/RocketsRepositoryImpl.kt
+++ RocketRepository/src/commonMain/kotlin/cz/quanti/razym/rocketropository/data/RocketsRepositoryImpl.kt
@@ -5,13 +5,15 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class RocketsRepositoryImpl(
-    private val api: SpaceXApi
+    private val api: SpaceXApi,
 ) : RocketsRepository {
-    override fun getRockets(): Flow<List<RocketData>> = flow {
-        emit(api.listRockets())
-    }
+    override fun getRockets(): Flow<List<RocketData>> =
+        flow {
+            emit(api.listRockets())
+        }
 
-    override fun getRocket(id: String): Flow<RocketData> = flow {
-        emit(api.getRocket(id))
-    }
+    override fun getRocket(id: String): Flow<RocketData> =
+        flow {
+            emit(api.getRocket(id))
+        }
 }
