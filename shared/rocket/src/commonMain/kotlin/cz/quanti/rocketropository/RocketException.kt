806ea34d27b5fcde6ed569035950a09a662ccd7b
Aleš Razým <ales.razym@quanti.cz>
2024-01-08 10:55:48 +0100

    FIX: Add `Result` tests, fix `RocketListViewModelTest`

--- shared/rocket/src/commonMain/kotlin/cz/quanti/rocketropository/RocketException.kt
+++ shared/rocket/src/commonMain/kotlin/cz/quanti/rocketropository/RocketException.kt
@@ -4,13 +4,28 @@ import io.ktor.http.HttpStatusCode
 
 sealed class RocketException(message: String, cause: Throwable?) : Throwable(message, cause) {
     class HttpException(
-        val statusCode: HttpStatusCode,
+        statusCode: HttpStatusCode,
         cause: Throwable?,
     ) : RocketException("HTTP error: $statusCode", cause)
 
-    class NetworkException(val errorMessage: String, cause: Throwable?) : RocketException(errorMessage, cause)
+    class NetworkException(errorMessage: String, cause: Throwable?) : RocketException(errorMessage, cause)
 
-    class ContentException(val errorMessage: String, cause: Throwable?) : RocketException(errorMessage, cause)
+    class ContentException(errorMessage: String, cause: Throwable?) : RocketException(errorMessage, cause)
 
-    class Exception(val errorMessage: String, cause: Throwable?) : RocketException(errorMessage, cause)
+    class Exception(errorMessage: String, cause: Throwable?) : RocketException(errorMessage, cause)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as RocketException
+
+        if (message != other.message) return false
+        if (cause != other.cause) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return this::class.hashCode()
+    }
 }
