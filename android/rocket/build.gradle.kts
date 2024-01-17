d9cf45c2e1cbd5bdcd630491ad4eb68b4c6206c6
Aleš Razým <ales.razym@quanti.cz>
2024-01-17 11:08:43 +0100

    REF: Rfc `RocketModule` into `rocket` module.

--- android/rocket/build.gradle.kts
+++ android/rocket/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.quanti.android.library)
+    alias(libs.plugins.quanti.android.library.compose)
     alias(libs.plugins.quanti.android.base)
     alias(libs.plugins.quanti.android.detekt)
     alias(libs.plugins.quanti.android.ktlint)
